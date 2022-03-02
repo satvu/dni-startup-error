f1dd4ef1270e77060f7dfbd84ab38b43c3b29fc1
Sarah Vu <sarahvu@microsoft.com>
2022-03-01 17:54:47 -0800

    dni app for investigating startup error

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using IsolatedStartupError;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace IsolatedStartupError
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("secrets.json")
+               .AddUserSecrets(Assembly.GetExecutingAssembly())
+               .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+        }
+    }
+}
