65b7d52af85e422b56ae2e270aa01b290caed979
Sarah Vu <sarahvu@microsoft.com>
2022-03-14 14:35:26 -0700

    edit where add user secrets is called

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.json", true, true)
+                        .AddUserSecrets(Assembly.GetExecutingAssembly());
+                })
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+/*                    services.AddOptions<ExampleServiceOptions>().Configure<IConfiguration>((settings, configuration) =>
+                    {
+                        configuration.GetSection(nameof(ExampleServiceOptions)).Bind(settings);
+                    });*/
+                })
+                .Build();
 
 host.Run();
